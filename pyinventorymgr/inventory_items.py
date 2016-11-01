76b0a84e0ebeb27c0eb9703575240c6f73b24f3a
Doyle Huang <doyle.sy.huang@mail.foxconn.com>
2016-11-01 09:52:55 +0800

    fix: CPU/DIMM inventory data still be persister even remove the device
    Change-Id: Icea9a86db0aa31aab1ed72305fcb54b0152b9745
    Signed-off-by: doyle.huang <doylehuang690712@gmail.com>
    Signed-off-by: Ken Lai <ken1029@gmail.com>
    Signed-off-by: doylehuang <doylehuang690712@gmail.com>
    Signed-off-by: Doyle Huang <doyle.sy.huang@mail.foxconn.com>

--- pyinventorymgr/inventory_items.py
+++ pyinventorymgr/inventory_items.py
@@ -24,42 +24,54 @@ class Inventory(DbusProperties, DbusObjectManager):
 
 
 class InventoryItem(DbusProperties):
-    def __init__(self, bus, name, data):
-        super(InventoryItem, self).__init__(
-            conn=bus,
-            object_path=name)
+	def __init__(self,bus,name,data):		
+		DbusProperties.__init__(self)
+		dbus.service.Object.__init__(self,bus,name)
 
-        self.name = name
+		self.name = name
+		
+		if (data.has_key('present') == False):
+			data['present'] = 'False'
+		if (data.has_key('fault') == False):
+			data['fault'] = 'False'
+		if (data.has_key('version') == False):
+			data['version'] = ''
 
-        if 'present' not in data:
-            data['present'] = 'False'
-        if 'fault' not in data:
-            data['fault'] = 'False'
-        if 'version' not in data:
-            data['version'] = ''
+		self.SetMultiple(INTF_NAME,data)
 
-        self.SetMultiple(INTF_NAME, data)
+		## this will load properties from cache
+		PropertyCacher.load(name, INTF_NAME, self.properties)
+		
+	@dbus.service.method(INTF_NAME,
+		in_signature='a{sv}', out_signature='')
+	def update(self,data):
+		self.SetMultiple(INTF_NAME,data)
+		PropertyCacher.save(self.name, INTF_NAME, self.properties)
 
-        ## this will load properties from cache
-        PropertyCacher.load(name, INTF_NAME, self.properties)
+	@dbus.service.method(INTF_NAME,
+		in_signature='s', out_signature='')
+	def setPresent(self,present):
+		self.Set(INTF_NAME,'present',present)
+		if (present == "True"):
+			self.Set(INTF_NAME,'present',present)
+		else:
+			data = FRUS[self.name.replace(System.INVENTORY_ROOT,"<inventory_root>")]
+			if (data.has_key('present') == False):
+				data['present'] = 'False'
+			if (data.has_key('fault') == False):
+				data['fault'] = 'False'
+			if (data.has_key('version') == False):
+				data['version'] = ''
+			self.properties[INTF_NAME] = {}
+			self.SetMultiple(INTF_NAME,data)
 
-    @dbus.service.method(
-        INTF_NAME, in_signature='a{sv}', out_signature='')
-    def update(self, data):
-        self.SetMultiple(INTF_NAME, data)
-        PropertyCacher.save(self.name, INTF_NAME, self.properties)
+		PropertyCacher.save(self.name, INTF_NAME, self.properties)
 
-    @dbus.service.method(
-        INTF_NAME, in_signature='s', out_signature='')
-    def setPresent(self, present):
-        self.Set(INTF_NAME, 'present', present)
-        PropertyCacher.save(self.name, INTF_NAME, self.properties)
-
-    @dbus.service.method(
-        INTF_NAME, in_signature='s', out_signature='')
-    def setFault(self, fault):
-        self.Set(INTF_NAME, 'fault', fault)
-        PropertyCacher.save(self.name, INTF_NAME, self.properties)
+	@dbus.service.method(INTF_NAME,
+		in_signature='s', out_signature='')
+	def setFault(self,fault):
+		self.Set(INTF_NAME,'fault',fault)
+		PropertyCacher.save(self.name, INTF_NAME, self.properties)
 
 
 def getVersion():
