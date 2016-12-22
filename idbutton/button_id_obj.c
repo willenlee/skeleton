32b19ee6cd8f994e79198907688f1bb6f8085ca7
Jason Luo <jason.cc.luo@mail.foxconn.com>
2016-12-22 09:56:38 +0800

    [Sthelens] Fix UID issue
    1. When UID LED is blinking, pressed UID button should light off UID LED.
    2. Fix UID button needs to press twice to disable UID LED after UID
       commad trigger UID LED.
    
    Change-Id: I347032afe55d54add82d1b1d0f12639d172ecd8c
    Signed-off-by: Jason Luo <jason.cc.luo@mail.foxconn.com>

--- idbutton/button_id_obj.c
+++ idbutton/button_id_obj.c
@@ -14,10 +14,18 @@ static const gchar* dbus_name = "org.openbmc.buttons.id";
 static const int LONG_PRESS_SECONDS = 3;
 static GDBusObjectManagerServer *manager = NULL;
 const char *power_ctrl = "brightness";
+const char *blink_ctrl = "trigger";
 
 //This object will use these GPIOs
-GPIO gpio_button = (GPIO){ "IDBTN" };
+GPIO gpio_button = (GPIO)
+{ "IDBTN"
+};
 
+enum {
+	UID_LIGHT_OFF,
+	UID_LIGHT_ON,
+	UID_LIGHT_BLINKING
+};
 int
 write_to_led_uid(const char *name, const char *ctrl_file, const char *value)
 {
@@ -29,15 +37,13 @@ write_to_led_uid(const char *name, const char *ctrl_file, const char *value)
 
 	int len = 0;
 	len = snprintf(led_path, sizeof(led_path),
-			"/sys/class/leds/%s/%s",name, ctrl_file);
-	if(len >= sizeof(led_path))
-	{
+		       "/sys/class/leds/%s/%s",name, ctrl_file);
+	if(len >= sizeof(led_path)) {
 		return rc;
 	}
 
 	FILE *fp = fopen(led_path,"w");
-	if(fp == NULL)
-	{
+	if(fp == NULL) {
 		return rc;
 	}
 
@@ -49,71 +55,101 @@ write_to_led_uid(const char *name, const char *ctrl_file, const char *value)
 	return (rc == 1) ? 0 : -1;
 }
 
+static int
+check_uidstate()
+{
+	FILE *fp1 = fopen("/sys/class/leds/identify/delay_on","r");
+	if(fp1 == NULL) {
+		int brightness = 0;
+		FILE *fp2 = fopen("/sys/class/leds/identify/brightness","r");
+		fread(&brightness, sizeof(int), 1, fp2);
+		fclose(fp2);
+
+		if(brightness == 0xa30) //0xa30 means ascii code '0' and 'new line', LSB first
+			return UID_LIGHT_OFF;
+		else
+			return UID_LIGHT_ON;
+	}
+
+	fclose(fp1);
+	return UID_LIGHT_BLINKING;//blinking timer exist
+}
+
 static gboolean
 on_button_interrupt( GIOChannel *channel,
-		GIOCondition condition,
-		gpointer user_data )
+		     GIOCondition condition,
+		     gpointer user_data )
 {
 	GError *error = 0;
 	gsize bytes_read = 0;
 	gchar buf[2];
-    static int light_on = 0;
+	static int light_on = 0;
 	buf[1] = '\0';
-    g_io_channel_seek_position( channel, 0, G_SEEK_SET, 0 );
+	g_io_channel_seek_position( channel, 0, G_SEEK_SET, 0 );
 	g_io_channel_read_chars(channel,
-			buf, 1,
-			&bytes_read,
-			&error ); 
+				buf, 1,
+				&bytes_read,
+				&error );
 
-	if(gpio_button.irq_inited)
-	{
-		if(buf[0] == '0')
-		{
-		    //button pressed
-		    printf("UID button pressed\n");
-            int rc = -1;
-            if (light_on == 0)
-            {
-                /*
-	                     * Before doing anything, need to turn off the blinking
-	                     * if there is one in progress by writing 'none' to trigger
-	                     */
-                rc = write_to_led_uid("identify", power_ctrl, "0");
-	            if(rc < 0)
-	            {
-		            fprintf(stderr,"Error disabling blink.\n");
-		            return TRUE;
-	            }
+	if(gpio_button.irq_inited) {
+		if(buf[0] == '0') {
+			//button pressed
+			printf("UID button pressed\n");
+			int rc = -1;
 
-                /*
-	                    * Open the brightness file and write corresponding values.
-	                    */
-                rc = write_to_led_uid("identify", power_ctrl, "255");
-	            if(rc < 0)
-	            {
-		            fprintf(stderr,"Error driving LED.\n");
-	            }
-                light_on = 1;
-            }
-            else
-            {
-                rc = write_to_led_uid("identify", power_ctrl, "0");
-	            if(rc < 0)
-	            {
-		            fprintf(stderr,"Error disabling blink.\n");
-		            return TRUE;
-	            }
-                light_on = 0;
-            }
-            
+			//check blinking
+			light_on = check_uidstate();
+
+			if (light_on == UID_LIGHT_BLINKING) {
+				//disable blinking
+				rc = write_to_led_uid("identify", blink_ctrl, "none");
+				if(rc < 0) {
+					fprintf(stderr,"Error disabling blink.\n");
+					return TRUE;
+				}
+
+				//light off led
+				rc = write_to_led_uid("identify", power_ctrl, "0");
+				if(rc < 0) {
+					fprintf(stderr,"Error disabling led.\n");
+					return TRUE;
+				}
+				light_on = UID_LIGHT_OFF;
+			} else if (light_on == UID_LIGHT_ON) {
+				rc = write_to_led_uid("identify", power_ctrl, "0");
+				if(rc < 0) {
+					fprintf(stderr,"Error disabling led.\n");
+					return TRUE;
+				}
+				light_on = UID_LIGHT_OFF;
+			} else {
+				/*
+				             * Before doing anything, need to turn off the blinking
+				             * if there is one in progress by writing 'none' to trigger
+				             */
+				rc = write_to_led_uid("identify", power_ctrl, "0");
+				if(rc < 0) {
+					fprintf(stderr,"Error disabling led.\n");
+					return TRUE;
+				}
+
+				/*
+				            * Open the brightness file and write corresponding values.
+				            */
+				rc = write_to_led_uid("identify", power_ctrl, "255");
+				if(rc < 0) {
+					fprintf(stderr,"Error driving LED.\n");
+				}
+				light_on = UID_LIGHT_ON;
+			}
+
+		} else {
+			//button released
+			printf("UID button released\n");
 		}
-		else
-		{
-		    //button released
-            printf("UID button released\n");
-		}
+	} else {
+		gpio_button.irq_inited = true;
 	}
-	else { gpio_button.irq_inited = true; }
 
 	return TRUE;
 }
@@ -144,27 +180,30 @@ on_bus_acquired(GDBusConnection *connection,
 	int rc = GPIO_OK;
 	do {
 		rc = gpio_init(connection,&gpio_button);
-		if(rc != GPIO_OK) { break; }
+		if(rc != GPIO_OK) {
+			break;
+		}
 		rc = gpio_open_interrupt(&gpio_button,on_button_interrupt,object);
-		if(rc != GPIO_OK) { break; }
+		if(rc != GPIO_OK) {
+			break;
+		}
 	} while(0);
-	if(rc != GPIO_OK)
-	{
+	if(rc != GPIO_OK) {
 		printf("ERROR PowerButton: GPIO setup (rc=%d)\n",rc);
 	}
 }
 
 static void
 on_name_acquired(GDBusConnection *connection,
-		const gchar *name,
-		gpointer user_data)
+		 const gchar *name,
+		 gpointer user_data)
 {
 }
 
 static void
 on_name_lost(GDBusConnection *connection,
-		const gchar *name,
-		gpointer user_data)
+	     const gchar *name,
+	     gpointer user_data)
 {
 }
 
@@ -181,14 +220,14 @@ main(gint argc, gchar *argv[])
 	loop = g_main_loop_new(NULL, FALSE);
 
 	id = g_bus_own_name(DBUS_TYPE,
-			dbus_name,
-			G_BUS_NAME_OWNER_FLAGS_ALLOW_REPLACEMENT |
-			G_BUS_NAME_OWNER_FLAGS_REPLACE,
-			on_bus_acquired,
-			on_name_acquired,
-			on_name_lost,
-			&cmd,
-			NULL);
+			    dbus_name,
+			    G_BUS_NAME_OWNER_FLAGS_ALLOW_REPLACEMENT |
+			    G_BUS_NAME_OWNER_FLAGS_REPLACE,
+			    on_bus_acquired,
+			    on_name_acquired,
+			    on_name_lost,
+			    &cmd,
+			    NULL);
 
 	g_main_loop_run(loop);
 
