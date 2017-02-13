8c543a5f1228ec69d3cf8808210565a5b1deb5a8
Jason Luo <jason.cc.luo@mail.foxconn.com>
2017-02-13 09:35:11 +0800

    [Sthelens] Modify GPU I2C bus
    Shift GPU bus to I2C bus 17 - I2C bus 20 because
    of I2C 9 - I2C 16 is used by PMBus.
    
    Change-Id: I5383e975b97c1b038efe1d4ef4acdfe4151a3c9e
    Signed-off-by: Jason Luo <jason.cc.luo@mail.foxconn.com>

--- gpu/gpu_core.c
+++ gpu/gpu_core.c
@@ -81,14 +81,14 @@ typedef struct {
 } gpu_device_mapping;
 
 gpu_device_mapping gpu_device_bus[MAX_GPU_NUM] = {
-	{9, 0x4e, EM_GPU_DEVICE_1},
-	{9, 0x4f, EM_GPU_DEVICE_3},
-	{10, 0x4e, EM_GPU_DEVICE_2},
-	{10, 0x4f, EM_GPU_DEVICE_4},
-	{11, 0x4e, EM_GPU_DEVICE_5},
-	{11, 0x4f, EM_GPU_DEVICE_7},
-	{12, 0x4e, EM_GPU_DEVICE_6},
-	{12, 0x4f, EM_GPU_DEVICE_8},
+	{17, 0x4e, EM_GPU_DEVICE_1},
+	{17, 0x4f, EM_GPU_DEVICE_3},
+	{18, 0x4e, EM_GPU_DEVICE_2},
+	{18, 0x4f, EM_GPU_DEVICE_4},
+	{19, 0x4e, EM_GPU_DEVICE_5},
+	{19, 0x4f, EM_GPU_DEVICE_7},
+	{20, 0x4e, EM_GPU_DEVICE_6},
+	{20, 0x4f, EM_GPU_DEVICE_8},
 };
 
 static int internal_gpu_access(int bus, __u8 slave,__u8 *write_buf, __u8 *read_buf)
