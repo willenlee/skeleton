5fdb80d3c73fead41c93f5090dc41fde6cfc5c16
Peter Lin <peter.cc.lin@mail.foxconn.com>
2017-06-01 18:13:32 +0800

    Implement Get Thermal Alert State command for GPU utility
    Change-Id: Ida41e73108a40c370fd779b355f0615cf5a6669c
    Signed-off-by: Peter Lin <peter.cc.lin@mail.foxconn.com>

--- gpu_utility/GPU_Utility.c
+++ gpu_utility/GPU_Utility.c
@@ -20,6 +20,7 @@
 #define NV_CMD_GET_GPU_INFORMATION    0x05
 #define NV_CMD_SET_RELEASE_THERMAL_ALERT    0xf4
 #define NV_CMD_GET_POWER_BARKE_STATE        0xf5
+#define NV_CMD_GET_THERMAL_ALERT_STATE      0xf6
 #define GPU_ACCESS_SUCCESS_RETURN 0x1f
 #define MAX_I2C_DEV_LEN 32
 #define PMBUS_DELAY usleep(400*1000)
@@ -326,6 +327,40 @@ static int get_power_brake_state(int gpu_no, int show_message)
 		printf("GPU power brake state:%d  \n",readbuf[0]);
 	return 0;
 }
+
+static int get_thermal_alert_state(int gpu_no, int show_message)
+{
+    unsigned char temp_writebuf[4] = {NV_CMD_GET_THERMAL_ALERT_STATE,0x0,0x0,0x80};
+	unsigned char readbuf[4];
+	int i2c_bus;
+	unsigned long slaveaddr;
+	int ret;
+	int gpu_index;
+
+	gpu_index = find_gpu_index(gpu_no);
+
+	if (gpu_index >= MAX_GPU_NUM || gpu_index == -1)
+	{
+		printf("Error gpu no:%d  exceed max gpu number:%d\n", gpu_no, MAX_GPU_NUM);
+		return -1;
+	}
+
+	i2c_bus = gpu_device_bus[gpu_index].bus_no;
+	slaveaddr = gpu_device_bus[gpu_index].slave;
+
+	ret = internal_gpu_access(i2c_bus, slaveaddr, temp_writebuf, readbuf);
+	if (ret != 0)
+	{
+		if (show_message == 1)
+			printf("Fail to get GPU[%d - %d 0x%x] thermal alert state Data: 0x%x 0x%x 0x%x 0x%x\n", gpu_index, i2c_bus, slaveaddr, readbuf[0], readbuf[1], readbuf[2], readbuf[3]);
+		return -1;
+	}
+
+	if (show_message == 1)
+		printf("GPU thermal alert state:%d  \n",readbuf[0]);
+	return 0;
+}
+
 void gpu_data_scan()
 {
 	int i =0;
@@ -604,6 +639,7 @@ usage(const char *prog)
 	       "\t-i [gpu number] :read GPU info\n"
 	       "\t-x [gpu number] [Arg1]: Set/Release the thermal alert\n"
 	       "\t-y [gpu number] :Get power brake state\n"
+	       "\t-z [gpu number] :Get thermal alert state\n"
 	       "\t-s scan GPU status\n"
 	       "\t-m [gpu number] [i2c number] [i2c slaveaddress] :set GPU map\n"
 	       "\t-p [i2c bus] [i2c slave address] [command code] [i2c data....] :block write i2c smbus command data\n"
@@ -634,7 +670,7 @@ int main(int argc, char **argv)
 	gpu_data_scan();
 	read_gpu_map();
 	
-	while ((opt = getopt(argc, argv, "hst:x:y:i:m:p:r:k:a:b:")) != -1) {
+	while ((opt = getopt(argc, argv, "hst:x:y:z:i:m:p:r:k:a:b:")) != -1) {
 		switch (opt) {
 		case 'h':
 			usage(argv[0]);
@@ -670,6 +706,10 @@ int main(int argc, char **argv)
 		case 'y':
 			gpu_no =  atoi(optarg);
 			get_power_brake_state(gpu_no, 1);
+			break;
+		case 'z':
+			gpu_no =  atoi(optarg);
+			get_thermal_alert_state(gpu_no, 1);
 			break;
 		case 's':
 			scan_gpu_status();
