c98dab8b9ac53788b3983acf11185c29c00926bc
Jason Luo <jason.cc.luo@mail.foxconn.com>
2017-06-30 16:49:02 +0800

    change GPU slave addr to 0x4d
    GPU will change to V100 on pvt
    
    Change-Id: I88501b1c8807a7cf89325c4df69302358ac7e108
    Signed-off-by: Jason Luo <jason.cc.luo@mail.foxconn.com>

--- gpu_utility/GPU_Utility.c
+++ gpu_utility/GPU_Utility.c
@@ -36,7 +36,7 @@
 #define MAX_CAPABILITIES_CMD_INDEX 5
 
 static int g_i2c_bus = 19;
-static unsigned long g_slaveaddr = 0x4f;
+static unsigned long g_slaveaddr = 0x4d;
 int fd;
 unsigned char cmd_reg[4];
 static int g_use_pec = 0;
@@ -68,14 +68,14 @@ typedef struct {
 } gpu_device_mapping;
 
 gpu_device_mapping gpu_device_bus[MAX_GPU_NUM] = {
-	{28, 0x4f, EM_GPU_DEVICE_1},
-	{29, 0x4f, EM_GPU_DEVICE_2},
-	{30, 0x4f, EM_GPU_DEVICE_3},
-	{31, 0x4f, EM_GPU_DEVICE_4},
-	{32, 0x4f, EM_GPU_DEVICE_5},
-	{33, 0x4f, EM_GPU_DEVICE_6},
-	{34, 0x4f, EM_GPU_DEVICE_7},
-	{35, 0x4f, EM_GPU_DEVICE_8},
+	{28, 0x4d, EM_GPU_DEVICE_1},
+	{29, 0x4d, EM_GPU_DEVICE_2},
+	{30, 0x4d, EM_GPU_DEVICE_3},
+	{31, 0x4d, EM_GPU_DEVICE_4},
+	{32, 0x4d, EM_GPU_DEVICE_5},
+	{33, 0x4d, EM_GPU_DEVICE_6},
+	{34, 0x4d, EM_GPU_DEVICE_7},
+	{35, 0x4d, EM_GPU_DEVICE_8},
 };
 
 static int internal_gpu_access(int bus, __u8 slave,__u8 *write_buf, __u8 *read_buf)
